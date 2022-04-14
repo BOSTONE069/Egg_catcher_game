a18ca5ddcbe8caf73ccce33ff02ea48847e1fd9f
rhemney <bostoneochieng69@gmail.com>
2022-04-14 10:13:28 -0700

    Final project

--- egg_catcher.py
+++ egg_catcher.py
@@ -21,7 +21,7 @@ egg_interval = 4000
 difficulty_factor = 0.95
 
 #This defines the outlook of the catcher and its colors and dimentions
-catcher_color = 'blue' 
+catcher_color = 'blue'
 catcher_width = 100
 catcher_height = 100
 catcher_start_x = canvas_width / 2 - catcher_width / 2
@@ -41,4 +41,78 @@ score_text = c.create_text(10, 10, anchor='nw', font=(
 lives_remaning = 3
 lives_text = c.create_text(canvas_width-10, 10, anchor='ne', font=(
     'Arial', 18, 'bold'), fill='darkblue', text='Lives : ' + str(lives_remaning))
+
+eggs = []
+
+#This defines the creation of the eggs
+def create_eggs():
+    x = randrange(10,740)
+    y = 40
+    new_egg = c.create_oval(x,y,x+egg_width,y+egg_height,fill=next(color_cycle),width=0)
+    eggs.append(new_egg)
+    win.after(egg_interval,create_eggs)
+
+#This defines the movement of the eggs
+def move_eggs():
+    for egg in eggs:
+        (egg_x,egg_y,egg_x2,egg_y2) = c.coords(egg)
+        c.move(egg,0,10)
+        if egg_y2 > canvas_height:
+            egg_dropped(egg)
+    win.after(egg_speed,move_eggs)
+
+#This defines the behaviour of the eggs
+def egg_dropped(egg):
+    eggs.remove(egg)
+    c.delete(egg)
+    lose_a_life()
+    if lives_remaning == 0:
+        messagebox.showinfo('GAME OVER!', 'Final Score : ' + str(score))
+        win.destroy()
+
+#This defines the behaviour of the lives score
+def lose_a_life():
+    global lives_remaning
+    lives_remaning -= 1
+    c.itemconfigure(lives_text, text='Lives : ' + str(lives_remaning))
+
+#This defines the behaviour of the of the catcher and the eggs
+def catch_check():
+    (catcher_x, catcher_y, catcher_x2, catcher_y2) = c.coords(catcher)
+    for egg in eggs:
+        (egg_x, egg_y, egg_x2, egg_y2) = c.coords(egg)
+        if catcher_x < egg_x and egg_x2 < catcher_x2 and catcher_y2 - egg_y2 < 40:
+            eggs.remove(egg)
+            c.delete(egg)
+            increase_score(egg_score)
+    win.after(100, catch_check)
+
+#This function help in determining the score and speed
+def increase_score(points):
+    global score, egg_speed, egg_interval
+    score += points
+    egg_speed = int(egg_speed * difficulty_factor)
+    egg_interval = int(egg_interval * difficulty_factor)
+    c.itemconfigure(score_text, text='Score : ' + str(score))
+
+# This fucnctions determines the movement of the catcher
+def move_left(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x1 > 0:
+        c.move(catcher, -20, 0)
+
+def move_right(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x2 < canvas_width:
+        c.move(catcher, 20, 0)
+
+#This bind the keys when pressed with the movenment of the catcher
+c.bind('<Left>', move_left)
+c.bind('<Right>', move_right)
+c.focus_set()
+
+win.after(1000,create_eggs)
+win.after(1000,move_eggs)
+win.after(1000,catch_check)
+
 win.mainloop()
